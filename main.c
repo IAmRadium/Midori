344308e5abcb17d4c5442aad6030ee8a66542dbc
Md Rasid Ali <rasid.crl@gmail.com>
2019-12-06 12:59:59 +0530

    Minor Changes required in MixColumn

--- main.c
+++ main.c
@@ -6,64 +6,55 @@ Md Rasid Ali
 
 /*Below Sbox is used for Midori128*/
 char Sb1[]={0x1,0x0,0x5,0x3,0xe,0x2,0xf,0x7,0xd,0xa,0x9,0xb,0xc,0x8,0x4,0x6};
-
+int shuffle[]={0,10,5,15,14,4,11,1,9,3,12,6,7,13,2,8};
+/*Defining MixColumn*//*DOUBTFUL*/
+void MixColumn(char state[]){
+	for(int i=0;i<4;i++){
+		state[i*4]=state[i*4+1]^state[i*4+2]^state[i*4+3];
+		state[i*4+1]=state[i*4]^state[i*4+2]^state[i*4+3];
+		state[i*4+2]=state[i*4]^state[i*4+1]^state[i*4+3];
+		state[i*4+3]=state[i*4]^state[i*4+1]^state[i*4+2];
+	}
+}
+/*Defining the Shuffle Cell Function*/
+void ShuffleCell(char state[]){
+	unsigned char temp;
+	for(int i=0;i<16;i++){
+		int swap_index=shuffle[i];
+		temp=state[swap_index];
+		state[swap_index]=state[i];
+		state[i]=temp;
+	}
+}
 /*Defining the AddRoundKey*/
 void AddRoundKey(char state[],char key[]){
 	for(int i=0;i<16;i++){
+		state[i]^=key[i];
 	}
 }
 /*defining the SubCell function*/
 void SSb0(char *cell){
-	//*cell=0xdb;
-	//printf("\n%d",*cell);
-	/*taking the 7th and 3rd bit*/
-	unsigned char temp1=0x88&(*cell);/*swap 7 with 3*/        //    y = (x << 2) | (x >> (8 - 2));
-	/*making circular shift to interchange the positions*/
-	temp1 = (temp1<<4) | (temp1>>4);
-	/*taking the 2th and 6rd bit*/
-	unsigned char temp2=0x22&(*cell);
-	temp2 = (temp2<<4) | (temp2>>4);
-	unsigned char result=0x55&(*cell);
-	result^=temp1;
-	result^=temp2;
+	unsigned char result=*cell;
+	result = (result&0x80)>>4 | (result&0x40)>>0 | (result&0x20)>>4 | (result&0x10)>>0 | (result&0x08)<<4 | (result&0x04)<<0 | (result&0x02)<<4 | (result&0x01)<<0;
+	result=Sb1[result&0x0f]^(Sb1[(result&0xf0)>>4])<<4;
+	result = (result&0x80)>>4 | (result&0x40)>>0 | (result&0x20)>>4 | (result&0x10)<<0 | (result&0x08)<<4 | (result&0x04)<<0 | (result&0x02)<<4 | (result&0x01)<<0;
 	*cell=result;
-	temp1=result&0x0f;//4bit LSB
-	temp1=Sb1[temp1];
-	temp2=result&0xf0;//
-	temp2=temp2>>4;
-	temp2=Sb1[temp2];
-	temp2=temp2<<4;
-	temp1=temp1^temp2;
-	*cell=temp1;
-	//printf("%d",*cell);
-	/*Doing the reverse bit swap*/
-	temp1=0x88&(*cell);/*swap 7 with 3*/
-	temp1 = (temp1<<4) | (temp1>>4);
-	temp2=0x22&(*cell);
-	temp2 = (temp2<<4) | (temp2>>4);
-	result=0x55&(*cell);
-	result^=temp1;
-	result^=temp2;
-	*cell=result;	
-	//printf("\n%d",result);
 }
 
 void SSb1(char *cell){
 	unsigned char result=*cell;
 	result = (result&0x80)>>3 | (result&0x40)<<1 | (result&0x20)>>3 | (result&0x10)>>3 | (result&0x08)>>3 | (result&0x04)<<1 | (result&0x02)<<5 | (result&0x01)<<5;
 	result=Sb1[result&0x0f]^(Sb1[(result&0xf0)>>4])<<4;
-	//printf("%d\n",result);
 	result = (result&0x80)>>1 | (result&0x40)>>5 | (result&0x20)>>5 | (result&0x10)<<3 | (result&0x08)>>1 | (result&0x04)<<3 | (result&0x02)<<3 | (result&0x01)<<3;
 	*cell=result;
-	printf("%d\n",result);	
 }
+
 void SSb2(char *cell){
 	unsigned char result=*cell;
 	result=(result&0x80)>>6 | (result&0x40)>>2 | (result&0x20)<<2 | (result&0x10)<<2 | (result&0x08)<<2 | (result&0x04)>>2 | (result&0x02)<<2 | (result&0x01)<<2;
 	result=Sb1[result&0x0f]^(Sb1[(result&0xf0)>>4])<<4;
 	result=(result&0x80)>>2 | (result&0x40)>>2 | (result&0x20)>>2 | (result&0x10)<<2 | (result&0x08)>>2 | (result&0x04)>>2 | (result&0x02)<<6 | (result&0x01)<<2;
 	*cell=result;
-	//printf("%d",result);
 }
 
 void SSb3(char *cell){
@@ -76,23 +67,25 @@ void SSb3(char *cell){
 
 void SubCell(char state[]){
 	for(int i=0;i<16;i++){
-		SSb0(&state[i]);
-		SSb1(&state[i]);
-		SSb2(&state[i]);
-		SSb3(&state[i]);
+		SSb0(&state[i]);SSb1(&state[i]);SSb2(&state[i]);SSb3(&state[i]);
 	}
 }
 int main(){
 	/*Defining the plaintext and key*/
-	char state[]={0x01,0x02,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-	char key[]={0x01,0x01,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+	char state[]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+	char key[]={0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
 	AddRoundKey(state,key);
-	/*for(int i=0;i<16;i++)
-		printf("%d",pt[i]);
-	*/
-	for(int i=0;i<15;i++){
+	for(int i=0;i<19;i++){
 		SubCell(state);
-		break;
+		ShuffleCell(state);
+		MixColumn(state);
+		AddRoundKey(state,key);
 	}
+	SubCell(state);
+	AddRoundKey(state,key);
+	for(int i=0;i<16;i++){
+		printf("%d",state[i]);
+	}
+	printf("\n");
 	return 0;
 }
